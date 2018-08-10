1d51d72f59d43558109107dad19247c796f83a45
Joshua Jones <joshuajones2010@gmail.com>
2018-08-10 16:34:57 +1000

    Add some comments

--- Assets/CustomProjectSettings/Scripts/Editor/CustomSettingsEditor.cs
+++ Assets/CustomProjectSettings/Scripts/Editor/CustomSettingsEditor.cs
@@ -12,27 +12,37 @@ namespace CustomProjectSettings
 
         private void OnEnable()
         {
+            //Store a reference to the settings file
             myTarg = (CustomSettingsBase)target;
+
+            //Hook into the undo performed function
             Undo.undoRedoPerformed += OnUndo;
         }
 
         private void OnDisable()
         {
+            //Remove the hook when not visible. Unity handles everything, don't worry!
             Undo.undoRedoPerformed -= OnUndo;
         }
 
         public override void OnInspectorGUI()
         {
-            EditorGUI.BeginChangeCheck();
-            base.OnInspectorGUI();
-            if (EditorGUI.EndChangeCheck())
+            //Listen for changes
+            using (var scope = new EditorGUI.ChangeCheckScope())
             {
-                myTarg.Save();
+                base.OnInspectorGUI();
+                if (scope.changed)
+                {
+                    //Save the file on change
+                    myTarg.Save();
+                }
             }
         }
 
         void OnUndo()
         {
+            //When Unity performs an Undo, it automatically reverts the changes.
+            //So all we need to do is save the file again
             myTarg.Save();
         }
     }
