522b97f58ef0eb1316cc1acf0de1ecf656646181
Joshua Jones <joshuajones2010@gmail.com>
2018-08-11 03:23:24 +1000

    Moved files. Added to ExampleSetting class to show that it work good

--- Assets/CustomProjectSettings/Demo/Scripts/ExampleSetting.cs
+++ Assets/CustomProjectSettings/Demo/Scripts/ExampleSetting.cs
@@ -2,7 +2,17 @@ using CustomProjectSettings;
 
 public class ExampleSetting : CustomSettings<ExampleSetting>
 {
+    [System.Serializable]
+    public class DemoClass
+    {
+        public string name;
+        public int someValue;
+    }
+
     public bool TEST;
+    public string SampleTextInput;
+    public DemoClass SampleClass;
+
 
     public override void OnWillSave() { }
     protected override void OnInitialise() { }
